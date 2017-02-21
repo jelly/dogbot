6853712cc31c07dfcacfbbee9aeaa74208a11fa8
Jelle van der Waa <jelle@vdwaa.nl>
2017-02-21 13:20:56 +0100

    initial commit

---
+++ 🐶.js
@@ -0,0 +1,26 @@
+const xdg = require('xdg-basedir')
+const irc = require('irc');
+
+const programName = 'dogbot';
+const configFile = `${xdg.config}/${programName}/config.json`
+const defaults = {
+	'server': 'irc.server.com',
+	'nick': 'doggie',
+	'channels': ['dogs']
+}
+const config = Object.assign({}, defaults, require(configFile));
+
+var client = new irc.Client(config.server, config.nick, {
+    channels: config.channels,
+});
+
+client.addListener('message', function (from, to, message) {
+    console.log(from + ' => ' + to + ': ' + message);
+    if (message.startsWith('dogbot')) {
+	    client.say(to, '🐶');
+    }
+});
+
+client.addListener('error', function(message) {
+    console.log('error: ', message);
+});
